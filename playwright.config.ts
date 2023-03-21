734dc65e2c8c2a02c9c29a8c05792bcbd2db52bf
Fernando Papito <papitorocks@gmail.com>
2023-03-21 12:05:22 -0300

    adicionando screenshots somente quando o teste falha

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL: 'https://loginxp.vercel.app'
+    baseURL: 'https://loginxp.vercel.app',
+    screenshot: 'only-on-failure'
   },
 
   /* Configure projects for major browsers */
